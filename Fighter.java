a208b6a40549338291e09b73ae0f3ef4aef1bbc0
Magnus Nording <120574513+MagNording@users.noreply.github.com>
2023-10-20 21:12:25 +0200

    Create Fighter.java

---
+++ Fighter.java
@@ -0,0 +1,22 @@
+public class Fighter {
+    private String name;
+    private int hp;
+
+    public Fighter(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+}
